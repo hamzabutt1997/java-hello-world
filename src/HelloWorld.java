ee81eb2f3ea7bb983c41144d730518267550fd61
MS2014-HAMZASHA\Hamza Shafique <bhamza786@gmail.com>
2026-03-05 19:31:22 +0500

    Change of Docker in Jenkins file

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("        Hello World from Jenkins!        ");
+        System.out.println("        Built with Docker Pipeline       ");
+        System.out.println("        Developer: Hamza Butt            ");
+        System.out.println("        Version: 1.0                     ");
+        System.out.println("=========================================");
+    }
+}
+```
+
+---
+
+# Steps
+```
+1. Click HelloWorld.java in VS Code
+2. Paste this code
+3. Press Ctrl+S to save
